1da868f65df3c15e23c2845c823b3d7c0dc3a700
Tamas Kurics <tamas.kurics@gmail.com>
2021-01-21 20:42:49 +0100

    add first Python files
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ Python/TextbookTrack/ba1a.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def count_pattern(text, pattern):
+    pattern_length = len(pattern)
+    count = 0
+    for ix in range(len(text)-pattern_length+1):
+        substring = text[ix:ix+pattern_length]
+        if substring == pattern:
+            count += 1
+    return count
+
+
+def main():
+    text, pattern = sys.stdin.read().splitlines()
+    result = count_pattern(text, pattern)
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
---
+++ Python/TextbookTrack/ba1b.py
@@ -0,0 +1,25 @@
+import sys
+
+from collections import Counter
+
+
+def most_frequent_k_mers(text, k):
+    counts = Counter()
+    for ix in range(len(text)-k+1):
+        k_mer = text[ix:ix+k]
+        counts.update([k_mer])
+
+    max_count = counts.most_common(n=1)[0][1]
+    return [k_mer for k_mer, count in counts.items() if count == max_count]
+
+
+def main():
+    data = sys.stdin.read().splitlines()
+    text = data[0]
+    k = int(data[1])
+    result = most_frequent_k_mers(text, k)
+    print(' '.join(result))
+
+
+if __name__ == '__main__':
+    main()
