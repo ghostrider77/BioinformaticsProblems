aaf403bd7bbe08583c86b179cb82071269b3eb98
Tamas Kurics <tamas.kurics@gmail.com>
2021-02-02 09:18:58 +0100

    add helper function for chapter 2 to the Julia folder
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ Julia/textbook_track/chapter02/ba2h.jl
@@ -0,0 +1,32 @@
+# Implement DistanceBetweenPatternAndStrings
+
+calc_hamming_distance(s1, s2) = count(((c1, c2),) -> c1 != c2, zip(s1, s2))
+
+
+function distance_between_pattern_and_text(text, pattern, k)
+    mindist = k
+    for ix in 1:(length(text)-k)
+        kmer = text[ix:ix+k-1]
+        distance = calc_hamming_distance(pattern, kmer)
+        if distance < mindist
+            mindist = distance
+        end
+    end
+    mindist
+end
+
+
+distance_between_pattern_and_collection_of_texts(texts, pattern, k) =
+    foldl((distance, text) -> distance + distance_between_pattern_and_text(text, pattern, k), texts; init=0)
+
+
+function main()
+    pattern = readline()
+    k = length(pattern)
+    texts = split(readline())
+    result = distance_between_pattern_and_collection_of_texts(texts, pattern, k)
+    println(result)
+end
+
+
+main()
