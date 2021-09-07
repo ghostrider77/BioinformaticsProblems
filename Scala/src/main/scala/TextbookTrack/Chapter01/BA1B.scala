36af97e2dccee70c4075f061256472cad63d92ce
Tamas Kurics <tamas.kurics@gmail.com>
2021-09-07 15:47:47 +0200

    fix variable naming convention-related typos
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

--- Scala/src/main/scala/TextbookTrack/Chapter01/BA1B.scala
+++ Scala/src/main/scala/TextbookTrack/Chapter01/BA1B.scala
@@ -3,14 +3,14 @@ package TextbookTrack.Chapter01
 object BA1B {
   def mostFrequentKMer(text: String, k: Int): List[String] = {
     val counts: Map[String, Int] = text.sliding(k).foldLeft(Map.empty[String, Int]) {
-      case (acc, k_mer) =>
-        val count: Int = acc.getOrElse(k_mer, 0)
-        acc.updated(k_mer, count + 1)
+      case (acc, kMer) =>
+        val count: Int = acc.getOrElse(kMer, 0)
+        acc.updated(kMer, count + 1)
     }
     val maxCount: Int = counts.valuesIterator.max
     counts
       .withFilter { case (_, count) => count == maxCount }
-      .map { case (k_mer, _) => k_mer }
+      .map { case (kMer, _) => kMer }
       .toList
   }
 
