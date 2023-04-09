f85f1f50b0b6e3c4277634728a4b65fe5f36bb0a
Tamas Kurics <tamas.kurics@gmail.com>
2023-04-09 17:57:13 +0200

    add missing type annotations
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

--- OCaml/src/stronghold/hamm.ml
+++ OCaml/src/stronghold/hamm.ml
@@ -1,4 +1,4 @@
-let calc_hamming_distance s1 s2 =
+let calc_hamming_distance (s1 : string) (s2 : string) : int =
     Seq.fold_left2 (fun acc c1 c2 -> if c1 <> c2 then acc + 1 else acc) 0 (String.to_seq s1) (String.to_seq s2)
 
 
