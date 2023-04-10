2349cd822cc5d57c27da7cc2c0d0f9b34b348dc8
Tamas Kurics <tamas.kurics@gmail.com>
2023-04-10 10:06:09 +0200

    add double-degree array problem in Ocaml
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

--- OCaml/src/algorithmic_heights/ddeg.ml
+++ OCaml/src/algorithmic_heights/ddeg.ml
@@ -5,11 +5,12 @@ type edge = { a : int; b : int }
 module Graph : sig
     type t
     val create : int -> edge list -> t
-    val degree : t -> int -> int
+    val get_neighbors : t -> int -> int list
+    val degrees : t -> int IntMap.t
 end = struct
     type t = { nr_nodes : int; adjacency_list : int list IntMap.t }
 
-    let get_neighbors adjacency_list node =
+    let get_neighbors {adjacency_list; _} node =
         match IntMap.find_opt node adjacency_list with
             | None -> []
             | Some neighbors -> neighbors
@@ -25,8 +26,8 @@ end = struct
         let adjacency_list = List.fold_left add_edge_both_way IntMap.empty edges in
         { nr_nodes; adjacency_list }
 
-    let degree {adjacency_list; _} node =
-        node |> get_neighbors adjacency_list |> List.length
+    let degrees {adjacency_list; _} =
+        IntMap.map List.length adjacency_list
 
 end
 
@@ -38,14 +39,19 @@ let read_edges (nr_edges : int) : edge list =
     List.of_seq @@ Seq.map (fun _ -> parse_line (read_line ())) range
 
 
-let calc_node_degrees (nr_nodes : int) (edges : edge list) : int list =
+let calc_double_degree_array (nr_nodes : int) (edges : edge list) : int list =
     let graph = Graph.create nr_nodes edges in
+    let degrees = Graph.degrees graph in
+    let get_degree node =
+        match IntMap.find_opt node degrees with
+            | None -> 0
+            | Some d -> d in
     let nodes = List.init nr_nodes (fun k -> k + 1) in
-    List.map (Graph.degree graph) nodes
+    List.map (fun node -> List.fold_left (fun acc n -> acc + get_degree n) 0 @@ Graph.get_neighbors graph node) nodes
 
 
 let () =
     let (nr_nodes, nr_edges) = Scanf.sscanf (read_line ()) "%d %d" (fun n e -> (n, e)) in
     let edge_list = read_edges nr_edges in
-    let result = calc_node_degrees nr_nodes edge_list in
+    let result = calc_double_degree_array nr_nodes edge_list in
     result |> List.map string_of_int |> String.concat " " |> print_endline
