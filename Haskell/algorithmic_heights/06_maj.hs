4c3cf08eb80ee7b025d61e393a3fb8649d85d2a6
Tamas Kurics <tamas.kurics@gmail.com>
2023-10-07 08:31:16 +0200

    find majority element
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ Haskell/algorithmic_heights/06_maj.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE TupleSections #-}
+
+import Control.Monad (replicateM)
+import Data.IntMap (IntMap, fromListWith)
+import qualified Data.IntMap as M
+
+
+convertToIntList :: String -> [Int]
+convertToIntList = map read . words
+
+
+readParameters :: String -> (Int, Int)
+readParameters line =
+    case convertToIntList line of
+        [a, b] -> (a, b)
+        _ -> error "Malformed input."
+
+
+readLists :: Int -> IO [[Int]]
+readLists k = do
+    rows <- replicateM k getLine
+    return $ map convertToIntList rows
+
+
+calcMajorityElements :: [[Int]] -> Int -> [Int]
+calcMajorityElements lists n =
+    let majorityelement xs =
+            let occurrences = fromListWith (+) $ map (, 1) xs
+                findMax item cnt (mostCommon, maxCount) =
+                    if cnt > maxCount then (item, cnt)
+                    else (mostCommon, maxCount)
+                (mostCommon, count) = M.foldrWithKey findMax (0, 0) occurrences
+            in if count > n `div` 2 then mostCommon else -1
+    in map majorityelement lists
+
+
+main :: IO ()
+main = do
+    (k, n) <- readParameters <$> getLine
+    lists <- readLists k
+    let result = calcMajorityElements lists n
+    putStrLn $ unwords $ map show result
