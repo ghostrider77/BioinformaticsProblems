c552ceab74b0465eb849946b75129c4fdf1b14d6
Tamas Kurics <tamas.kurics@gmail.com>
2023-10-14 06:29:14 +0200

    insertion sort with in-place swaps
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ Haskell/algorithmic_heights/04_ins.hs
@@ -0,0 +1,33 @@
+import Control.Monad (foldM)
+import Control.Monad.ST (runST)
+import Data.Vector (Vector, fromList, thaw)
+import qualified Data.Vector.Mutable as MV
+
+
+convertToIntVector :: String -> Vector Int
+convertToIntVector = fromList . map read . words
+
+
+calcInsertionSortSwaps :: Vector Int -> Int -> Int
+calcInsertionSortSwaps vector n =
+    let go array nrSwaps k = do
+            if k == 0 then return nrSwaps
+            else do
+                x <- MV.read array k
+                y <- MV.read array (k - 1)
+                if x >= y then return nrSwaps
+                else do
+                    MV.swap array k (k - 1)
+                    go array (nrSwaps + 1) (k - 1)
+        getAllSwaps array = foldM (go array) 0 [1..(n-1)]
+    in runST $ do
+        array <- thaw vector
+        getAllSwaps array
+
+
+main :: IO ()
+main = do
+    n <- readLn
+    vector <- convertToIntVector <$> getLine
+    let result = calcInsertionSortSwaps vector n
+    print result
